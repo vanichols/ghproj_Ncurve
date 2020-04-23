a09f0ecd7e2d0a668de81fe7b4716661c34d9d18
Unknown <vnichols@iastate.edu>
2020-04-23 11:59:48 -0500

    hea fixed something

--- code/01_proc-raw-dat.R
+++ code/01_proc-raw-dat.R
@@ -10,17 +10,19 @@ library(dplyr)
 library(stringr)
 library(janitor)
 library(readxl)
+
 # data by year ------------------------------------------------------------
 
 
 raw2 <- read_excel("data/raw/20200421_apsimdatacomplete.xlsx") %>% 
   rename(site = pub_reference) %>% 
   mutate_if(is.character, tolower) %>% 
-  mutate_if(is.character, stringr::str_trim)
-
-raw2 %>% write_csv("data/tidy/td_crop-by-year.csv")
-
-
+  mutate_if(is.character, stringr::str_trim) %>% 
+  mutate(site_id = recode(site,
+                       "randall.iragavarapu" = "rair"),
+         site_id = stringr::str_sub(site, 1, 4))
+                       
+                       
 # look at it --------------------------------------------------------------
 
 #--use pwalk to make a fig for each site
@@ -58,5 +60,37 @@ leach %>%
 
 #--need to think about the metrics we want
 
-soi <- read_csv("data/raw/soilallsites.csv")
+soi <- read_csv("data/raw/soilallsites.csv") %>% 
+  rename(site = pub_reference) %>% 
+  mutate_if(is.character, tolower) %>% 
+  mutate_if(is.character, stringr::str_trim) %>% 
+  mutate(site_id = recode(site,
+                          "randall.iragavarapu" = "rair_rand_iragav"),
+         site_id = stringr::str_sub(site_id, 1, 4))
 
+
+#--mean values for 0-60cm
+soi_60cm <- 
+  soi %>% 
+  select(site, site_id, depth_inc, depth_cat, om_pct:pH, -texture) %>%
+  pivot_longer(om_pct:pH) %>% 
+  filter(depth_cat < 7) %>% 
+  group_by(site, site_id, name) %>% 
+  summarise(mn_val = mean(value, na.rm = T))
+
+soi_60cm %>% 
+  ggplot(aes(site_id, mn_val)) + 
+  geom_point() + 
+  facet_wrap(~name, scales = "free") + 
+  coord_flip()
+
+#--lots of stuff is useless
+#--need to do something diff for paw, but ok for now
+
+soi_tidy <- 
+  soi_60cm %>% 
+  filter(name %in% c("BD", "clay_pct", "DUL", "KS", "LL", "om_pct", "PAW", "pH")) %>% 
+  pivot_wider(names_from = name, values_from = mn_val)
+
+
+soi_tidy %>% write_csv("data/tidy/td_soil-60cm.csv")
